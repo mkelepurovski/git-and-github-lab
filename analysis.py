a07cbc3331482c7f6d559ab99110d83147af84c6
mkelepurovski <mike5175@colorado.edu>
2026-07-14 10:58:18 -0600

    Added largest sale amount

--- analysis.py
+++ analysis.py
@@ -2,3 +2,4 @@ import pandas as pd
 df=pd.read_csv("C:/Users/mkele/Downloads/sales.csv")
 print("Number of Rows:", len(df))
 print("Mean sale amount:", df["amount"].mean())
+print("Largest sale amount", df["amount"].max())
