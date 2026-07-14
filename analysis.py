7a248f7405cf335890979ed0304f9ee2020cbdf8
mkelepurovski <mike5175@colorado.edu>
2026-07-14 11:18:39 -0600

    Some colons were missing, I added them, yay!

--- analysis.py
+++ analysis.py
@@ -1,5 +1,5 @@
 import pandas as pd
 df=pd.read_csv("C:/Users/mkele/Downloads/sales.csv")
-print("Number of Rows:", len(df))
-print("Mean sale amount:", df["amount"].mean())
-print("Largest sale amount", df["amount"].max())
+print("N rows:", len(df))
+print("Mean sale:", df["amount"].mean())
+print("Largest sale:", df["amount"].max())
